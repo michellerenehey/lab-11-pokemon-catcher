4af3bfdc664d7212bfce9948afd4593239436662
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-20 16:55:54 -0700

    added setPokedex and encounterPokemon function with Julie's help

--- storageUtils.js
+++ storageUtils.js
@@ -1,3 +1,4 @@
+// find by ID
 export function findById(id, itemList) {
     for (let item of itemList) {
         if (item.id === id){
@@ -6,16 +7,41 @@ export function findById(id, itemList) {
     }
 }
 
-export function getResults(){
+// set pokedex
+export function setPokedex(pokemonList){
+    const resultsString = JSON.stringify(pokemonList); 
+    localStorage.setItem('RESULTS', resultsString); 
+
+}
+
+// get pokedex
+export function getPokedex(){
     const resultsString = localStorage.getItem('RESULTS') || '[]'; 
     const results = JSON.parse(resultsString); 
     return results;
 }
 
-export function encountered(){
-
+// encounter
+    
+    // call getPokedex() 
+    // find the matching pokemon (findByID)
+    // increment encounter (these are shown)
+    // localStorage.setItem
+    
+export function encounterPokemon(id){
+    const results = getPokedex(); 
+    const encountered = findById(id, results); 
+    if (encountered) {
+        encountered.encounter++; 
+    } else {
+        const newEncounter = { id: id, encounter: 1, capture: }; 
+        results.push(newEncounter); 
+    }
+    const resultsString = JSON.stringify(results); 
+    localStorage.setItem('RESULTS', resultsString)
 }
 
-export function selected(){
-    
+// capture 
+export function capturePokemon(id){
+    // increement what is chosen/preferred/caught
 }
