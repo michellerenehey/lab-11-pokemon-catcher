0977ed23ece86846f209b43cbb5070a08326ce92
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-21 15:49:42 -0700

    rendered pokemon from results array onto results.js

--- results/results.js
+++ results/results.js
@@ -1,3 +1,27 @@
 import { pokemonList } from '../pokemon.js'; 
+import { findById, getPokedex } from '../storageUtils.js'; 
 
-const resultsShown 
+const results = getPokedex(); 
+const resultsHolder = document.getElementById('results-section'); 
+
+for (let item of results){
+    const pokefriend = findById(item.id, pokemonList); 
+
+    const div = document.createElement('div'); 
+
+    const pokeName = document.createElement('h2'); 
+    pokeName.textContent = pokefriend.pokemon; 
+
+    const pokeImg = document.createElement('img'); 
+    pokeImg.src = pokefriend.url_image;
+
+    const encounterSpan = document.createElement('span'); 
+    encounterSpan.textContent = `Times encountered: ${item.encounter}`; 
+
+    const captureSpan = document.createElement('span'); 
+    captureSpan.textContent = `Times captured: ${item.capture}`; 
+
+    div.append(pokeName, pokeImg, encounterSpan, captureSpan); 
+    resultsHolder.append(div); 
+
+}
