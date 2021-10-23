3e36d91fe74ee54c543932327520128a8c48a167
Michelle Nygren <michelle.r.nygren@gmail.com>
2021-10-22 17:15:43 -0700

    added test for findbyid function

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
-import { getPokedex, encounterPokemon, capturePokemon, setPokedex } from '../storageUtils.js';
+import { getPokedex, encounterPokemon, capturePokemon, setPokedex, findById } from '../storageUtils.js';
+import { pokemonList } from '../pokemon.js';
 
 const test = QUnit.test;
 
@@ -109,3 +110,44 @@ test('setPokedex should send data up to localStorage', (expect) => {
     //Expect
     expect.deepEqual(actual, results);
 }); 
+
+// TEST 7 findByID should return the poke matching the id
+test('findById should return the poke matching the id', (expect) => {
+
+    const expected = {
+        '_id':'5cef3501ef6005a77cd4fd17',
+        'pokemon':'bulbasaur',
+        'id':1,
+        'species_id':1,
+        'height':7,
+        'weight':69,
+        'base_experience':64,
+        'type_1':'grass',
+        'type_2':'poison',
+        'attack':49,
+        'defense':49,
+        'hp':45,
+        'special_attack':65,
+        'special_defense':65,
+        'speed':45,
+        'ability_1':'overgrow',
+        'ability_2':'NA',
+        'ability_hidden':'chlorophyll',
+        'color_1':'#78C850',
+        'color_2':'#A040A0',
+        'color_f':'#81A763',
+        'egg_group_1':'monster',
+        'egg_group_2':'plant',
+        'url_image':'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        'generation_id':1,
+        'evolves_from_species_id':'NA',
+        'evolution_chain_id':1,
+        'shape_id':8,
+        'shape':'quadruped',
+        'pokebase':'bulbasaur',
+        'pokedex':'http://www.pokemon.com/us/pokedex/bulbasaur'
+    };
+
+    const actual = findById(1, pokemonList); 
+    expect.deepEqual(actual, expected);
+});
